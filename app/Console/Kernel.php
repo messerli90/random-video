c758f622640d8d011e29f9ebad9caf0d56f7766e
Michael Messerli <michaelamesserli@gmail.com>
2017-03-12 15:47:50 -0600

    :package: Schedule command

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,6 +27,7 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')
         //          ->hourly();
+        $schedule->command('videos:get')->twiceDaily();
     }
 
     /**
